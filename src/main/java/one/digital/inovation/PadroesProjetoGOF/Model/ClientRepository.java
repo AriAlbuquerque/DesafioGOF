bb63c5eb039fe82888b7108b23c81e38851abf5a
Ariane Albuquerque <arianeoalbuquerques@gmail.com>
2023-04-27 19:54:11 -0300

    "Atualização do projeto"

--- src/main/java/one/digital/inovation/PadroesProjetoGOF/Model/ClientRepository.java
+++ src/main/java/one/digital/inovation/PadroesProjetoGOF/Model/ClientRepository.java
@@ -3,7 +3,10 @@ package one.digital.inovation.PadroesProjetoGOF.Model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends CrudRepository<Cliente, Long> {
 
+    Optional<Cliente> findAllById(Long id);
 }
--- src/main/java/one/digital/inovation/PadroesProjetoGOF/Service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digital/inovation/PadroesProjetoGOF/Service/impl/ClienteServiceImpl.java
@@ -1,21 +1,37 @@
 package one.digital.inovation.PadroesProjetoGOF.Service.impl;
 
+import one.digital.inovation.PadroesProjetoGOF.Model.ClientRepository;
 import one.digital.inovation.PadroesProjetoGOF.Model.Cliente;
+import one.digital.inovation.PadroesProjetoGOF.Model.EnderecoRepository;
 import one.digital.inovation.PadroesProjetoGOF.Service.ClientService;
+import one.digital.inovation.PadroesProjetoGOF.Service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+
     @Override
     public Iterable<Cliente> buscarTodos(){
-        return null;
+        return clientRepository.findAll();
     }
     @Override
     public Cliente buscarPorId(Long id){
-        return null;
+      Optional<Cliente> cliente = clientRepository.findAllById(id);
+        return cliente.get();
     }
     @Override
     public void inserir(Cliente cliente){
+
     }
     @Override
     public void atualizar(Long id, Cliente cliente){
